463ee58983201efc567c626b945525f36f77151a
Mihalea Mircea <mm8g15@soton.ac.uk>
2015-10-06 13:56:46 +0100

    Started writing unit tests.

---
+++ src/test/java/barebones/logic/InterpreterTest.java
@@ -0,0 +1,36 @@
+package barebones.logic;
+
+import barebones.events.ErrorResponse;
+import barebones.events.EventResponse;
+import junit.framework.TestCase;
+
+/**
+ * Created by mm8g15 on 06/10/2015.
+ */
+public class InterpreterTest extends TestCase {
+
+    public void testListenerCreation() throws Exception {
+        assertNotNull(getListener());
+    }
+
+    public void testTimeout() throws Exception {
+        String code = "clear x; incr x; while x not 0 do; end;";
+        EventResponse response = getListener().compile(code);
+
+        assertNotNull(response);
+
+        if(response instanceof ErrorResponse == false)
+            fail("Interpreter did not exit with an error");
+        else {
+            ErrorResponse errorResponse = (ErrorResponse) response;
+            if(errorResponse.errors.size() == 0)
+                fail("No errors contained in the event");
+        }
+    }
+
+    private Listener getListener() {
+        Interpreter interpreter = new Interpreter();
+        Listener listener = interpreter.setupListener();
+        return listener;
+    }
+}
