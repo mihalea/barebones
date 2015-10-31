17df9e607596ec337e96ad6c3dc3480e795abd3a
Mircea Mihalea <mm8g15@soton.ac.uk>
2015-10-31 01:23:42 +0000

    More tests

--- src/test/java/ro/mihalea/cadets/barebones/logic/FetcherTest.java
+++ src/test/java/ro/mihalea/cadets/barebones/logic/FetcherTest.java
@@ -9,17 +9,75 @@ import java.util.List;
  */
 public class FetcherTest extends TestCase {
 
-    public void testAdd() throws Exception {
+    public void testAddSingleStatement() throws Exception {
         Fetcher fetcher = new Fetcher();
         assertNotNull(fetcher);
 
-        fetcher.add("incr x");
+        fetcher.add("incr x;");
         List<String> instr = fetcher.fetch();
         assertEquals(1, instr.size());
         assertEquals("incr x", instr.get(0));
 
         instr = fetcher.fetch();
         assertEquals(0, instr.size());
+    }
 
+    public void testSameLineStatements() throws Exception {
+        Fetcher fetcher = new Fetcher();
+        assertNotNull(fetcher);
+
+        fetcher.add("incr x; decr x;");
+        List<String> instr = fetcher.fetch();
+        assertEquals(2, instr.size());
+        assertEquals("incr x", instr.get(0));
+        assertEquals("decr x", instr.get(1));
+
+        instr = fetcher.fetch();
+        assertEquals(0, instr.size());
+    }
+
+    public void testMultipleLines() throws Exception {
+        Fetcher fetcher = new Fetcher();
+        assertNotNull(fetcher);
+
+        fetcher.add("incr x;");
+        fetcher.add("decr x; clear y;");
+        List<String> instr = fetcher.fetch();
+        assertEquals(3, instr.size());
+        assertEquals("incr x", instr.get(0));
+        assertEquals("decr x", instr.get(1));
+        assertEquals("clear y", instr.get(2));
+
+        instr = fetcher.fetch();
+        assertEquals(0, instr.size());
+    }
+
+    public void testClearAfterAdd() throws Exception {
+        Fetcher fetcher = new Fetcher();
+        assertNotNull(fetcher);
+
+        fetcher.add("incr x; decr x;");
+        fetcher.clear();
+        List<String> instr = fetcher.fetch();
+        assertEquals(0, instr.size());
+    }
+
+    public void testAddAfterClear() throws Exception {
+        Fetcher fetcher = new Fetcher();
+        assertNotNull(fetcher);
+
+        fetcher.clear();
+        fetcher.add("decr x;");
+        List<String> instr = fetcher.fetch();
+        assertEquals(1, instr.size());
+        assertEquals("decr x", instr.get(0));
+    }
+
+    public void testClearOnEmpty() throws Exception {
+        Fetcher fetcher = new Fetcher();
+        assertNotNull(fetcher);
+
+        fetcher.clear();
+        assertEquals(0, fetcher.fetch().size());
     }
 }
--- src/test/java/ro/mihalea/cadets/barebones/logic/InterpreterTest.java
+++ src/test/java/ro/mihalea/cadets/barebones/logic/InterpreterTest.java
@@ -1,15 +1,6 @@
 package ro.mihalea.cadets.barebones.logic;
 
-import ro.mihalea.cadets.barebones.events.ErrorResponse;
-import ro.mihalea.cadets.barebones.events.EventResponse;
-import ro.mihalea.cadets.barebones.events.ResultResponse;
 import junit.framework.TestCase;
-import ro.mihalea.cadets.barebones.logic.BonesError;
-import ro.mihalea.cadets.barebones.logic.Interpreter;
-import ro.mihalea.cadets.barebones.logic.Listener;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by mm8g15 on 06/10/2015.
