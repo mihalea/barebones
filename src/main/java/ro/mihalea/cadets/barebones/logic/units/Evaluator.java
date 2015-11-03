393257d2313ff25515ca8e9d28f649d3f57a74bd
Mircea Mihalea <mm8g15@soton.ac.uk>
2015-11-03 00:32:56 +0000

    Documented the evaluator

--- src/main/java/ro/mihalea/cadets/barebones/logic/units/Evaluator.java
+++ src/main/java/ro/mihalea/cadets/barebones/logic/units/Evaluator.java
@@ -12,7 +12,7 @@ import java.util.Stack;
 import java.util.regex.Pattern;
 
 /**
- * Created by Mircea on 02-Nov-15.
+ * Class used for evaluating expressions
  */
 public class Evaluator {
     /**
@@ -30,33 +30,58 @@ public class Evaluator {
      */
     private static String RX_OPERATOR = "[" + Pattern.quote("+-*/()") + "]";
 
+    /**
+     * Memory on which to apply actions
+     */
     private Memory memory;
 
+    /**
+     * All evaluators must take a Memory object
+     * @param memory Memory on which to work
+     */
     public Evaluator(Memory memory) {
         this.memory = memory;
     }
 
+    /**
+     * Calculates the value of the expression if possible
+     * @param elements Queue of values and operators in infix notation
+     * @return Result of the expression
+     * @throws NotAssignedException Value not assigned before using
+     * @throws InvalidCharacterException Invalid character used as an operator
+     * @throws InvalidExpressionException Invalid expression
+     */
     public long evaluate(LinkedList<String> elements) throws NotAssignedException, InvalidCharacterException,
             InvalidExpressionException {
+        //Values get stored here until they are used
         Stack<Long> values = new Stack<>();
+        //Operators are stored here until they are used
         Stack<Character> operators = new Stack<>();
+        //Copy the values so that we can use the same instruction multiple times
         LinkedList<String> input = new LinkedList<>(elements);
 
+        //Current token
         String token;
         while(!input.isEmpty()) {
             token = input.remove();
+
             if(Evaluator.isNumber(token))
+                //If number push it to value stack
                 values.push(Long.parseLong(token));
             else if(Evaluator.isVariable(token))
+                //If variable get value and push it to value stack
                 values.push(memory.get(token));
             else if(Evaluator.isOperator(token)) {
                 char operator = token.charAt(0);
 
                 switch (operator) {
                     case '(':
+                        //If open parenthesis push it to operator stack
                         operators.push(operator);
                         break;
                     case ')':
+                        //If closed parenthesis remove all operators from the queue until the pair is
+                        //encountered and then dispose both of them
                         while(!operators.isEmpty() && operators.peek() != '(')
                             values.push(operate(values.pop(), values.pop(), operators.pop()));
                         if(operators.peek() == '(')
@@ -65,6 +90,7 @@ public class Evaluator {
                             throw new InvalidExpressionException("Unmatched paranthesis");
                         break;
                     default:
+                        //If not parenthesis, then it must be an operator, so use it on the value queue
                         while(!operators.isEmpty() && precedence(operators.peek()) >= precedence(operator)) {
                             values.push(operate(values.pop(), values.pop(), operators.pop()));
                         }
@@ -75,17 +101,27 @@ public class Evaluator {
             }
         }
 
+        //Operate on all remaining operators
         while(!operators.isEmpty()) {
             values.push(operate(values.pop(), values.pop(), operators.pop()));
         }
 
+        //If there are more values remaining, then the expression is invalid
         if(values.size() != 1)
             throw new InvalidExpressionException();
 
         return values.peek();
     }
 
-    public long operate(long a, long b, char operator) throws InvalidCharacterException {
+    /**
+     *
+     * @param a First term
+     * @param b Second term
+     * @param operator Operator to be used
+     * @return Value of the operation
+     * @throws InvalidCharacterException Invalid character used as an operator
+     */
+    private long operate(long a, long b, char operator) throws InvalidCharacterException {
         switch (operator){
             case '+':
                 return b+a;
@@ -100,18 +136,38 @@ public class Evaluator {
         }
     }
 
+    /**
+     * Returns whether the string is a valid variable name
+     * @param variable Variable name to check
+     * @return True if the naming meets the specification
+     */
     public static boolean isVariable(String variable) {
         return Pattern.matches(RX_NAME, variable);
     }
 
+    /**
+     * Returns whether the string is a number
+     * @param numeric Number to check
+     * @return True if the string is a valid number
+     */
     public static boolean isNumber(String numeric) {
         return Pattern.matches(RX_NUMBER, numeric);
     }
 
+    /**
+     * Returns whether the string is a valid operator
+     * @param operator Operator to check
+     * @return True if the operator is valid
+     */
     public static boolean isOperator(String operator) {
         return Pattern.matches(RX_OPERATOR, operator);
     }
 
+    /**
+     * Returns the precedence level of the operator
+     * @param operator Operator to be used
+     * @return Precedence level
+     */
     private int precedence(char operator) {
         switch(operator) {
             case '*':
